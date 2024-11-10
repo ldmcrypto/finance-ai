f476b3208b8177fe9584ccf4ac7bce6ae03b6096
ldmcrypto <leandrodubinski@gmail.com>
2024-11-10 15:01:41 -0300

    chore: add shudcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
+import {Button} from "./_components/ui/button";
+
 const HOME = () => {
-  return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500"> hello wolrd</h1>
-    </div>
-  );
+  return <Button> Hello world </Button>;
+
 };
 
 export default HOME;
