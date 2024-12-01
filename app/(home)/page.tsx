9ba081a5a1de837f0222a0e9529193d55a218d7f
ldmcrypto <leandrodubinski@gmail.com>
2024-12-01 13:56:13 -0300

    feat: modfi

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,23 +22,23 @@ const HOME = async ({ searchParams: { month } }: HomeProps) => {
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
     redirect("?month=1");
-   
   }
 
   const dashboard = await getDashboard(month);
-
 
   return (
     <>
       <Navbar />
       <div className="space-y-6 p-6">
-        <div className="flex justify-between"></div>
-        <div className="grid-cols-[2fr, 1fr] grid">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6">
-          <TimeSelect /> 
             <SummaryCards month={month} {...dashboard} />
             <div className="grid grid-cols-3 grid-rows-1 gap-6">
-            
               <TransactionsPieChart {...dashboard} />
             </div>
           </div>
